bf16744689ba08fbae87f3e3172a632568743581
kina28rbp <kinasiti177@gmail.com>
2026-05-10 15:55:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -52,14 +52,14 @@ questions = [
         "jawaban": "c. Keimin Bunka Sidhoso"
     },
     {
-        "soal": "Dampak pendudukan Jepang di Indonesia dalam bidang ekonomi adalah?"
+        "soal": "Dampak pendudukan Jepang di Indonesia dalam bidang ekonomi adalah?",
         "opsi": [
-            "a. dikenalnya berbagai macam tanaman ekspor",
-            "b. membaiknya kesejahteraan petani",
-            "c. makin menurunnya produksi pertanian",
-            "d. meningkatkan produksi pertanian",
-            "e. awal kegiatan ekspor hasil bumi",
-            ], 
+             "a. dikenalnya berbagai macam tanaman ekspor",
+             "b. membaiknya kesejahteraan petani",
+             "c. makin menurunnya produksi pertanian",
+             "d. meningkatkan produksi pertanian",
+             "e. awal kegiatan ekspor hasil bumi"
+         ],
         "jawaban": "c. makin menurunnya produksi pertanian"
     },
     {
@@ -67,6 +67,7 @@ questions = [
         "opsi": ["Kloroplas", "Ribosom", "Vakuola"],
         "jawaban": "Kloroplas"
     }
+]
 st.title("🚀 Smart-Sprint AI")
 
 menu = st.sidebar.selectbox("Pilih Menu", ["Home", "Baca Materi", "Mulai Kuis"])
